74e67098cd4468d2d1d422b160d570c5fc2d677a
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-12-02 08:31:43 +0300

    working with comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [num for num in num_list if num % 2 == 0]
+    print(num_list)
+    return num_list
+
+return_evens(num_list=range(1,10,2))
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [sentence + '!' for sentence in sentence_list]
+    print (new_list)
+    return new_list
+make_exclamation([])
